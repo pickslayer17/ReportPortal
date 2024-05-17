b6cf5f643dbebc011a1429ba12f973280d9f0171
Dzianis Piatrovich <dzianis.piatrovich@intapp.com>
2024-05-17 17:43:57 +0300

    pravki jeni

--- ReportPortal/Controllers/LoginController.cs
+++ ReportPortal/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using Models.Dto;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 namespace ReportPortal.Controllers
@@ -18,8 +19,9 @@ namespace ReportPortal.Controllers
             _config = config;
         }
 
+
         [AllowAnonymous]
-        [HttpPost]
+        [HttpPost("Login")]
         public IActionResult Login([FromBody]UserDto login)
         {
             IActionResult response = null;// Unauthorized();
@@ -34,15 +36,30 @@ namespace ReportPortal.Controllers
             return response;
         }
 
+
+
+        [Authorize(Roles ="Admin")]
+        [HttpPost("Test")]
+        public IActionResult Test()
+        {
+
+            return Ok();
+        }
+
         private string GenerateJSONWebToken(UserDto userInfo)
         {
             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
             var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new List<Claim>();
 
+            claims.Add(new Claim("Name", "Denis"));
+
+            claims.Add(new Claim(ClaimTypes.Role, "Admin"));
+            
             var token = new JwtSecurityToken(
                 _config["Jwt:Issuer"],
                 _config["Jwt:Issuer"],
-                null,
+                claims,
                 expires: DateTime.Now.AddMinutes(120),
                 signingCredentials: credentials);
 
