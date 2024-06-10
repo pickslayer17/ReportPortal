784926f4458097a0cbd1dc581236a939579e53d7
Dzianis Piatrovich <dzianis.piatrovich@gmail.com>
2024-06-10 10:42:12 +0300

    add field for FolderVm

--- ReportPortal/ViewModels/TestRun/FolderVm.cs
+++ ReportPortal/ViewModels/TestRun/FolderVm.cs
@@ -2,6 +2,11 @@
 {
     public class FolderVm
     {
+        public int Id { get; set; }
+        public int RunId { get; set; }
+        public string Name { get; set; }
+        public Nullable<int> ParentId { get; set; }
+        public List<int>? ChildFolderIds { get; set; }
+        public List<int>? TestIds { get; set; }
     }
-
 }
