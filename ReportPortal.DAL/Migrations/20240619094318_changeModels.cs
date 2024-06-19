ef1c7b247d4e4c440e15fe9b02656c6883e4e220
Dzianis Piatrovich <dzianis.piatrovich@intapp.com>
2024-06-19 12:43:52 +0300

    add migration

---
+++ ReportPortal.DAL/Migrations/20240619094318_changeModels.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReportPortal.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TestResultIds",
+                table: "Tests");
+
+            migrationBuilder.DropColumn(
+                name: "RunId",
+                table: "TestResults");
+
+            migrationBuilder.DropColumn(
+                name: "RootFolderId",
+                table: "Runs");
+
+            migrationBuilder.DropColumn(
+                name: "RunIds",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "ChildFolderIds",
+                table: "Folders");
+
+            migrationBuilder.DropColumn(
+                name: "RunId",
+                table: "Folders");
+
+            migrationBuilder.DropColumn(
+                name: "TestIds",
+                table: "Folders");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tests_FolderId",
+                table: "Tests",
+                column: "FolderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestResults_TestId",
+                table: "TestResults",
+                column: "TestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Runs_ProjectId",
+                table: "Runs",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Folders_ParentId",
+                table: "Folders",
+                column: "ParentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Folders_Folders_ParentId",
+                table: "Folders",
+                column: "ParentId",
+                principalTable: "Folders",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Runs_Projects_ProjectId",
+                table: "Runs",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TestResults_Tests_TestId",
+                table: "TestResults",
+                column: "TestId",
+                principalTable: "Tests",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tests_Folders_FolderId",
+                table: "Tests",
+                column: "FolderId",
+                principalTable: "Folders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Folders_Folders_ParentId",
+                table: "Folders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Runs_Projects_ProjectId",
+                table: "Runs");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_TestResults_Tests_TestId",
+                table: "TestResults");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tests_Folders_FolderId",
+                table: "Tests");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tests_FolderId",
+                table: "Tests");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TestResults_TestId",
+                table: "TestResults");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Runs_ProjectId",
+                table: "Runs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Folders_ParentId",
+                table: "Folders");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TestResultIds",
+                table: "Tests",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RunId",
+                table: "TestResults",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RootFolderId",
+                table: "Runs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RunIds",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ChildFolderIds",
+                table: "Folders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RunId",
+                table: "Folders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TestIds",
+                table: "Folders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
