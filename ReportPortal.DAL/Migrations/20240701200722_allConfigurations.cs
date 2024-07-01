223f649a1e6df2021678a9561471c3e2a3e011f8
Dzianis Piatrovich <dzianis.piatrovich@gmail.com>
2024-07-01 23:07:41 +0300

    add all configurations

---
+++ ReportPortal.DAL/Migrations/20240701200722_allConfigurations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReportPortal.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class allConfigurations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
