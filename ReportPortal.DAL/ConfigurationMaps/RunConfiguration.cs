489f35f15643f109ed42f3d420e39ee096e3ccce
Dzianis Piatrovich <dzianis.piatrovich@gmail.com>
2024-09-08 14:55:18 +0300

    fix

--- ReportPortal.DAL/ConfigurationMaps/RunConfiguration.cs
+++ ReportPortal.DAL/ConfigurationMaps/RunConfiguration.cs
@@ -15,7 +15,7 @@ namespace ReportPortal.DAL.ConfigurationMaps
             builder.Property(e => e.ProjectId).HasColumnName("ProjectId").HasColumnType("int");
             builder.HasOne(e => e.Project).WithMany(e => e.Runs).HasForeignKey(e => e.ProjectId).OnDelete(deleteBehavior: DeleteBehavior.ClientCascade);
             builder.Property(e => e.RootFolderId).HasColumnName("RootFolderId").HasColumnType("int");
-            builder.HasOne(e => e.RootFolder).WithOne(e => e.Run).HasForeignKey<Folder>(e => e.RunId).OnDelete(DeleteBehavior.ClientCascade);
+            builder.HasOne(e => e.RootFolder).WithOne(e => e.Run).HasForeignKey<RootFolder>(e => e.RunId).OnDelete(DeleteBehavior.ClientCascade);
             builder.HasMany(e => e.Folders).WithOne(e => e.Run).HasForeignKey(e => e.RunId).OnDelete(DeleteBehavior.ClientCascade);
         }
     }
