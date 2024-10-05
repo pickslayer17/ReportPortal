1186831ea3f5312cefd86abc5899d4ec3fef4d48
Dzianis Piatrovich <dzianis.piatrovich@gmail.com>
2024-10-05 17:00:27 +0300

    working with weatherForecast

--- ReactFE/reactfe.client/vite.config.ts
+++ ReactFE/reactfe.client/vite.config.ts
@@ -30,9 +30,6 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
     }
 }
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7289';
-
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
@@ -43,8 +40,8 @@ export default defineConfig({
     },
     server: {
         proxy: {
-            '^/ReportPortal': {
-                target,
+            '^/weatherforecast': {
+                target: 'https://localhost:7073/',
                 secure: false
             }
         },
