852eadc12ff2ca5d68af0d26771a45bc1181f734
Dzianis Piatrovich <dzianis.piatrovich@gmail.com>
2024-11-09 17:00:39 +0200

    add migration for productBug

---
+++ ReportPortal.DAL/Migrations/20241109150007_TestReviewProductBug.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReportPortal.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestReviewProductBug : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductBug",
+                schema: "dbo",
+                table: "TestReviews",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductBug",
+                schema: "dbo",
+                table: "TestReviews");
+        }
+    }
+}
