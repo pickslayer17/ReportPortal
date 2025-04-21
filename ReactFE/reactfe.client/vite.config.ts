cdd029613ceb283f387b88e8f49f89f5675dfeed
Dzianis Piatrovich <dzianis.piatrovich@intapp.com>
2025-04-21 19:54:10 +0300

    update vite config

--- ReactFE/reactfe.client/vite.config.ts
+++ ReactFE/reactfe.client/vite.config.ts
@@ -1,55 +1,25 @@
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig, loadEnv } from 'vite'
+import plugin from '@vitejs/plugin-react'
 
-import { defineConfig } from 'vite';
-import plugin from '@vitejs/plugin-react';
-import fs from 'fs';
-import path from 'path';
-import child_process from 'child_process';
-import { env } from 'process';
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '') 
 
-const baseFolder =
-    env.APPDATA !== undefined && env.APPDATA !== ''
-        ? `${env.APPDATA}/ASP.NET/https`
-        : `${env.HOME}/.aspnet/https`;
-
-const certificateName = "reactfe.client";
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
-
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit', }).status) {
-        throw new Error("Could not create certificate.");
-    }
-}
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [plugin()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    },
-    server: {
-        proxy: {
-            '^/login': {
-                target: 'https://localhost:7073/',
-                secure: false
+    return {
+        plugins: [plugin()],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
             }
         },
-        port: 5173,
-        host: true, 
-        https: {
-            key: fs.readFileSync(keyFilePath),
-            cert: fs.readFileSync(certFilePath),
+        server: {
+            port: 5173,
+            host: true
+        },
+        define: {
+            'import.meta.env.VITE_API_URL': JSON.stringify(
+                process.env.VITE_API_URL ?? env.VITE_API_URL
+            )
         }
     }
 })
