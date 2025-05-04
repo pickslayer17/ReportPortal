768c6978cf4de456fe2f2c29fe759feb9a521a4a
Dzianis Piatrovich <dzianis.piatrovich@gmail.com>
2025-05-04 22:33:03 +0300

    change config to npm start locally

--- ReactFE/reactfe.client/vite.config.ts
+++ ReactFE/reactfe.client/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => {
             }
         },
         server: {
-            port: 5173,
+            port: 100,
             host: true
         },
         define: {
